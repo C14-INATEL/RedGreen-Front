ee10a0a7c5a4b97e93fc57f91d92ba37d5aca3cb
Pedro Andrade <phpa_psa@hotmail.com>
2026-03-18 16:06:37 -0300

    fix: changed background color to index.css and added an image background

--- src/presentation/pages/Home.tsx
+++ src/presentation/pages/Home.tsx
@@ -1,11 +1,8 @@
 const Home = () => {
   return (
-    // Container principal com a cor de fundo e o padrão de naipe
     <div
       className="relative w-screen h-screen overflow-hidden suit-pattern"
-      style={{ background: 'hsl(150 40% 12%)' }}
     >
-      {/* Gradiente radial que brilha do centro para fora */}
       <div
         className="absolute inset-0 pointer-events-none"
         style={{
@@ -13,8 +10,6 @@ const Home = () => {
             'radial-gradient(ellipse at center, hsl(150 35% 16% / 0.6) 0%, transparent 70%)',
         }}
       />
-
-      {/* Gradiente de escurecimento na parte inferior da tela */}
       <div
         className="absolute bottom-0 left-0 right-0 h-20 pointer-events-none"
         style={{
