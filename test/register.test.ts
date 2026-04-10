e88fc6b92ef52fd3db3429af48e6070f72052117
Danilo3105 <187675324+DaniloSilva31@users.noreply.github.com>
2026-04-09 23:29:10 -0300

    style: transform into english and standardize the messages

--- test/register.test.ts
+++ test/register.test.ts
@@ -1,16 +1,16 @@
 import { IsValidBirthDate } from '../src/validators';
 import { describe, expect, it } from '@jest/globals';
 
-describe('Validação de data de nascimento', () => {
-  it('Valid date', () => {
+describe('isValidBirthDate', () => {
+  it('valid date', () => {
     expect(IsValidBirthDate('01/01/2000')).toBe(true);
   });
 
-  it('Invalid date', () => {
+  it('invalid date', () => {
     expect(IsValidBirthDate('31/02/2000')).toBe(false);
   });
 
-  it('Future date', () => {
+  it('future date', () => {
     expect(IsValidBirthDate('01/01/3000')).toBe(false);
   });
 });
