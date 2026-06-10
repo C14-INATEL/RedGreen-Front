558178fbf0c0f8b2749216b9878dedb88ea3f6e9
Danilo3105 <187675324+DaniloSilva31@users.noreply.github.com>
2026-06-10 14:11:28 -0300

    refactor: message from admin indicating a desk is locked

--- src/presentation/ui/SlotMachineCard.tsx
+++ src/presentation/ui/SlotMachineCard.tsx
@@ -70,7 +70,7 @@ export const SlotMachineCard = ({
         {MinimumChipsRequired.toLocaleString('pt-BR')}
       </p>
 
-      {IsLocked && !IsAdmin && (
+      {IsLocked && (
         <div className="absolute inset-0 flex items-center justify-center bg-black/60">
           <span
             className="text-[9px] uppercase text-red-400"
