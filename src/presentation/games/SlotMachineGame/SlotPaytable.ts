fb2aaf17d039d98651b4ab4f4241ec30230a7ef8
Danilo3105 <187675324+DaniloSilva31@users.noreply.github.com>
2026-06-12 19:30:30 -0300

    style: the `npm run format` command was applied

--- src/presentation/games/SlotMachineGame/SlotPaytable.ts
+++ src/presentation/games/SlotMachineGame/SlotPaytable.ts
@@ -56,7 +56,7 @@ export const SLOT_PAYTABLE_ENTRIES: readonly SlotPaytableEntry[] = [
   {
     id: 'cheese',
     prize: '+10 por rato',
-    symbols: [SLOT_SYMBOL_SOURCES.cheese,"+",SLOT_SYMBOL_SOURCES.rat],
+    symbols: [SLOT_SYMBOL_SOURCES.cheese, '+', SLOT_SYMBOL_SOURCES.rat],
   },
   {
     id: 'egg',
